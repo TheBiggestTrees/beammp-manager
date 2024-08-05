bbcc78bf49e931f7ac57ad3c341a7b5086ab507f
Tiirnako <treeleaves229@gmail.com>
2024-08-05 14:27:47 -0500

    fix layout on maps and include default maps

--- src/renderer/components/Maps.jsx
+++ src/renderer/components/Maps.jsx
@@ -6,12 +6,31 @@ function Maps() {
   const [maps, setMaps] = useState([]);
   const [selectedMap, setSelectedMap] = useState('');
 
+  const defaultMaps = [
+    'gridmap',
+    'gridmap_v2',
+    'automation_test_track',
+    'east_coast_usa',
+    'hirochi_raceway',
+    'italy',
+    'industrial',
+    'small_island',
+    'smallgrid',
+    'utah',
+    'west_coast_usa',
+    'driver_training',
+    'derby',
+    'jungle_rock_island',
+    'johnson_valley',
+  ];
+
   useEffect(() => {
     window.electron.ipcRenderer
       .getMaps()
       .then((res) => {
         console.log(res);
-        setMaps(res);
+        const result = [...defaultMaps, ...res];
+        return setMaps(result);
       })
       .catch((err) => console.error(err));
     window.electron.ipcRenderer
@@ -29,15 +48,16 @@ function Maps() {
   };
 
   return (
-    <div className="flex flex-col h-[100vh] bg-gray-500 px-4 py-4">
+    <div className="flex flex-col bg-gray-500 px-4 py-4">
       <div className="flex flex-row gap-2">
         <LinkButton to="" text="Home" />
         <LinkButton to="settings" text="Settings" />
       </div>
 
       <h1 className="text-3xl text-white font-bold mb-2">Maps</h1>
-      <span>{selectedMap}</span>
-      <div className="flex flex-col gap-2">
+      <span>Selected: {selectedMap}</span>
+      <span>Total: {maps.length}</span>
+      <div className="flex flex-col gap-2 h-[78vh] overflow-auto no-scrollbar">
         {maps.map((map) => (
           <div
             className="flex items-center justify-center bg-white border-2 border-black rounded-lg"
