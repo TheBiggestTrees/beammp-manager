6c3bc8544e803f8f0b2e1c6ed4e509179cbc7f11
Tiirnako <treeleaves229@gmail.com>
2024-08-05 15:06:04 -0500

    fix shrink separator for generalserversettings

--- src/renderer/components/GeneralServerSettings.jsx
+++ src/renderer/components/GeneralServerSettings.jsx
@@ -4,7 +4,7 @@ function GeneralServerSettings() {
       <h2 className="text-2xl text-white font-bold mb-2">
         General Server Settings
       </h2>
-      <div className="bg-black rounded-lg h-[3px]" />
+      <div className="bg-black rounded-lg h-[2px]" />
       <div>
         <span className="text-white">Not Implemented</span>
       </div>
